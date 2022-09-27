105f6470f9f0d02319cb41f17c08899ac4f56549
Scarax <simone.scaratti@bluemail.it>
2022-09-27 17:19:09 -0500

    Repaired files in static folder.

---
+++ ChatApp/src/main/resources/static/login.js
@@ -0,0 +1,14 @@
+function redirect() {
+    var value = document.getElementById("usernameBox").value;
+
+    if (value == "admin1" || value == "admin2" || value == "admin3" || value == "admin4" || value == "admin5") {
+        document.getElementById("loginButton").setAttribute("href", "landingPageAdmin.html");
+    }
+    else if (value === "john") {
+        document.getElementById("loginButton").setAttribute("href", "landingPageUser.html");
+    }
+    else {
+        document.getElementById("loginButton").setAttribute("href", "#");
+        alert("Information is incorrect.");
+    }
+}
